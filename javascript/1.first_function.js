7f95d2cd8d91f642574f97e490dffc6a337859a8
manggong <jihwank1995@gmail.com>
2019-12-22 23:30:02 +0900

    javascript_tutorial

---
+++ javascript/1.first_function.js
@@ -0,0 +1,6 @@
+function sayHello(apple, banana) {
+    console.log('Hello!', apple, "I have", banana);
+}
+
+
+sayHello("kim", 25);
--- javascript/test.js
+++
@@ -1,7 +0,0 @@
-const hello = {
-    name : "kim",
-    age : 23,
-    gender : "Male"
-}
-
-console.log(hello.name);
