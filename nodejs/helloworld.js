de4f1232720284eb8613a316dd321d2747740510
manggong <jihwank1995@gmail.com>
2020-01-20 19:40:10 +0900

    20200120

---
+++ nodejs/helloworld.js
@@ -0,0 +1,24 @@
+function Promise1() {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      resolve("hello");
+    }, 5000);
+  });
+}
+
+function Promise2() {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      resolve("world");
+    }, 2000);
+  });
+}
+
+async function helloWorld() {
+  const result1 = await Promise1();
+  console.log(result1);
+  const result2 = await Promise2();
+  console.log(result2);
+}
+
+helloWorld();
