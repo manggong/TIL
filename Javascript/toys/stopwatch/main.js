0d42a629dac8b432ac76528cc88653ff606ffb8a
manggong <jihwank1995@gmail.com>
2021-05-20 21:30:16 +0900

    stop watch

---
+++ Javascript/toys/stopwatch/main.js
@@ -0,0 +1,49 @@
+let seconds = 00;
+let tens = 00;
+const appendTens = document.querySelector('#tens');
+const appendSeconds = document.querySelector('#seconds');
+const buttonStart = document.querySelector('#start');
+const buttonStop = document.querySelector('#stop');
+const buttonReset = document.querySelector('#reset');
+let Interval;
+
+buttonStart.onclick = function () {
+  clearInterval(Interval);
+  Interval = setInterval(startTimer, 10);
+};
+
+buttonStop.onclick = function () {
+  clearInterval(Interval);
+};
+
+buttonReset.onclick = function () {
+  clearInterval(Interval);
+  tens = '00';
+  seconds = '00';
+  appendTens.innerHTML = tens;
+  appendSeconds.innerHTML = seconds;
+};
+
+function startTimer() {
+  tens++;
+
+  if (tens <= 9) {
+    appendTens.innerHTML = '0' + tens;
+  }
+
+  if (tens > 9) {
+    appendTens.innerHTML = tens;
+  }
+
+  if (tens > 99) {
+    console.log('seconds');
+    seconds++;
+    appendSeconds.innerHTML = '0' + seconds;
+    tens = 0;
+    appendTens.innerHTML = '0' + 0;
+  }
+
+  if (seconds > 9) {
+    appendSeconds.innerHTML = seconds;
+  }
+}
